603d673c5c491ff8a4c8f3f313129a6e1e95c967
Suraj Rawat <surajsinghrawat1999@gmail.com>
2023-07-24 12:20:31 +0530

    added if the place exist or not function , added images,

--- backend.py
+++ backend.py
@@ -1,14 +1,16 @@
 import requests
-
 API_KEY = "803a47834b9fad266400965ea41ec5aa"
+
+
 def get_data(place, forecast_days=None, kind=None):
 
 
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
-
+    nr_values = 8 * forecast_days
+    filtered_data = data["list"]
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
