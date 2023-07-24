ddf6099f927641c3c82b3ee9f31595f43697b382
Suraj Rawat <surajsinghrawat1999@gmail.com>
2023-07-24 11:43:32 +0530

    get place json file from website api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast For The Next Days")
 place = st.text_input("Place: ")
@@ -9,11 +10,12 @@ days = st.slider("Forecast days", min_value=1,
 option = st.selectbox("Select data to view",
                       ("Temperature", "sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
 
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+d, t = get_data(place, days, option)
+
+d, t = get_data(days)
+
+
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
